cae9c4c2ac6171a1b3c1b0c921d5de7057df443e
aablagait <aleksej.bocharov9@gmail.com>
2024-12-14 08:21:45 +0300

    проект готов, здесь просто детектируют людей на видео

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import math
 
 
 # start webcam
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('tester.mov')
 cap.set(3, 640)
 cap.set(4, 480)
 
@@ -24,42 +25,43 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
-
+frame_count = 0
 while True:
     success, img = cap.read()
-    results = model(img, stream=True, )
+    if frame_count % 30 == 0:
+        results = model(img, )
     # coordinates
-    for r in results:
+        for r in results:
 
-        boxes = r.boxes
+            boxes = r.boxes
 
-        for box in boxes:
-            cls = int(box.cls[0])
-            # for print only 'person'
-            if classNames[cls] == "person":
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
-
-                # put box in cam
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-                # confidence
-                confidence = math.ceil((box.conf[0]*100))/100
-                print("Confidence --->",confidence)
-
-                # class name
+            for box in boxes:
                 cls = int(box.cls[0])
-                print("Class name -->", classNames[cls])
+                # for print only 'person'
+                if classNames[cls] == "person":
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-                # object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
+                    # put box in cam
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                    # confidence
+                    confidence = math.ceil((box.conf[0]*100))/100
+                    print("Confidence --->",confidence)
+
+                    # class name
+                    cls = int(box.cls[0])
+                    print("Class name -->", classNames[cls])
+
+                    # object details
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (255, 0, 0)
+                    thickness = 2
+
+                    cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
